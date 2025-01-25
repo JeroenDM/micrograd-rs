fd616e597a28725e486c6b68513213b5f18a9c69
jeroendm <jeroendemaeyer@live.be>
2025-01-25 09:25:14 +0100

    move to lib, add tests and reset function

---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+use std::{cell::Cell, rc::Rc};
+
+/// Wrapper type to allow storing the nodes in a graph
+/// without having to wrap it into an Rc manually everywhere.
+#[derive(Debug, Clone)]
+pub struct Value(Rc<_Value>);
+
+#[derive(Debug)]
+struct _Value {
+    data: f64,
+    grad: Cell<f64>,
+    done: Cell<bool>,
+    op: Op,
+}
+
+/// Enum describing a directed acyclic computation graph.
+#[derive(Debug, Clone)]
+enum Op {
+    Constant,
+    Add(Value, Value),
+    Mul(Value, Value),
+}
+
+impl Value {
+    pub fn new(data: f64) -> Self {
+        Self(Rc::new(_Value {
+            data,
+            grad: Cell::new(0.0),
+            done: Cell::new(false),
+            op: Op::Constant,
+        }))
+    }
+
+    pub fn data(&self) -> f64 {
+        self.0.data
+    }
+
+    pub fn grad(&self) -> f64 {
+        self.0.grad.get()
+    }
+
+    pub fn set_grad(&self, x: f64) {
+        self.0.grad.set(x)
+    }
+
+    pub fn get_data(&self) -> f64 {
+        self.0.data
+    }
+
+    pub fn backwards(&self) {
+        if !self.0.done.get() {
+            self.0.done.set(true);
+            let g_parent = self.0.grad.get();
+            match &self.0.op {
+                Op::Constant => (),
+                Op::Add(x, y) => {
+                    x.set_grad(x.grad() + g_parent);
+                    y.set_grad(y.grad() + g_parent);
+                    x.backwards();
+                    y.backwards();
+                }
+                Op::Mul(x, y) => {
+                    x.0.grad.set(x.0.grad.get() + y.0.data * g_parent);
+                    y.0.grad.set(y.0.grad.get() + x.0.data * g_parent);
+                    x.backwards();
+                    y.backwards();
+                }
+            }
+        }
+    }
+
+    pub fn reset(&self) {
+        if self.0.done.get() {
+            self.set_grad(0.0);
+            self.0.done.set(false);
+            match &self.0.op {
+                Op::Constant => (),
+                Op::Add(x, y) => {
+                    x.reset();
+                    y.reset();
+                }
+                Op::Mul(x, y) => {
+                    x.reset();
+                    y.reset();
+                }
+            }
+        }
+    }
+}
+
+pub fn add(x: &Value, y: &Value) -> Value {
+    Value(Rc::new(_Value {
+        data: x.get_data() + y.get_data(),
+        grad: Cell::new(0.0),
+        done: Cell::new(false),
+        op: Op::Add(x.clone(), y.clone()),
+    }))
+}
+
+pub fn mul(x: &Value, y: &Value) -> Value {
+    Value(Rc::new(_Value {
+        data: x.get_data() * y.get_data(),
+        grad: Cell::new(0.0),
+        done: Cell::new(false),
+        op: Op::Mul(x.clone(), y.clone()),
+    }))
+}
+
+pub fn neg(x: &Value) -> Value {
+    mul(x, &Value::new(-1.0))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ops_forward() {
+        let a = Value::new(10.0);
+        let b = Value::new(3.0);
+        assert_eq!(neg(&a).data(), -10.0);
+        assert_eq!(add(&a, &b).data(), 13.0);
+        assert_eq!(mul(&a, &b).data(), 30.0);
+    }
+
+    #[test]
+    fn add_backwards() {
+        let a = Value::new(10.0);
+        let b = Value::new(3.0);
+        let c = add(&a, &b);
+        c.set_grad(2.0);
+        c.backwards();
+        assert_eq!(a.grad(), 2.0);
+        assert_eq!(b.grad(), 2.0);
+    }
+
+    #[test]
+    fn mul_backwards() {
+        let a = Value::new(10.0);
+        let b = Value::new(3.0);
+        let c = mul(&a, &b);
+        c.set_grad(2.0);
+        c.backwards();
+        assert_eq!(a.grad(), 6.0);
+        assert_eq!(b.grad(), 20.0);
+    }
+
+    #[test]
+    fn add_mul_neg_backwards() {
+        let a = Value::new(10.0);
+        let b = Value::new(3.0);
+        let c = add(&a, &neg(&mul(&a, &b)));
+        c.set_grad(3.0);
+        c.backwards();
+        assert_eq!(a.grad(), -6.0);
+        assert_eq!(b.grad(), -30.0);
+    }
+
+    #[test]
+    fn add_mul_neg_backwards_reset() {
+        let a = Value::new(10.0);
+        let b = Value::new(3.0);
+        let c = add(&a, &neg(&mul(&a, &b)));
+
+        c.set_grad(1.0);
+        c.backwards();
+        assert_eq!(a.grad(), -2.0);
+        assert_eq!(b.grad(), -10.0);
+
+        c.reset();
+        c.set_grad(1.0);
+        c.backwards();
+        assert_eq!(a.grad(), -2.0);
+        assert_eq!(b.grad(), -10.0);
+
+        c.reset();
+        c.set_grad(2.0);
+        c.backwards();
+        assert_eq!(a.grad(), -4.0);
+        assert_eq!(b.grad(), -20.0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,4 @@
-use std::{cell::Cell, rc::Rc};
-
-#[derive(Debug, Clone)]
-enum Op {
-    Constant,
-    Add(Value, Value),
-    Mul(Value, Value),
-}
-
-#[derive(Debug, Clone)]
-struct Value(Rc<_Value>);
-
-#[derive(Debug)]
-struct _Value {
-    data: f64,
-    grad: Cell<f64>,
-    done: Cell<bool>,
-    op: Op,
-}
-
-impl Value {
-    fn new(data: f64) -> Self {
-        Self(Rc::new(_Value {
-            data,
-            grad: Cell::new(0.0),
-            done: Cell::new(false),
-            op: Op::Constant,
-        }))
-    }
-
-    fn get_data(&self) -> f64 {
-        self.0.data
-    }
-
-    fn backwards(&self) {
-        if !self.0.done.get() {
-            self.0.done.set(true);
-            let g_parent = self.0.grad.get();
-            match &self.0.op {
-                Op::Constant => (),
-                Op::Add(x, y) => {
-                    x.0.grad.set(x.0.grad.get() + g_parent);
-                    y.0.grad.set(y.0.grad.get() + g_parent);
-                    x.backwards();
-                    y.backwards();
-                }
-                Op::Mul(x, y) => {
-                    x.0.grad.set(x.0.grad.get() + y.0.data * g_parent);
-                    y.0.grad.set(y.0.grad.get() + x.0.data * g_parent);
-                    x.backwards();
-                    y.backwards();
-                }
-            }
-        }
-    }
-}
-
-fn add(x: &Value, y: &Value) -> Value {
-    Value(Rc::new(_Value {
-        data: x.get_data() + y.get_data(),
-        grad: Cell::new(0.0),
-        done: Cell::new(false),
-        op: Op::Add(x.clone(), y.clone()),
-    }))
-}
-
-fn mul(x: &Value, y: &Value) -> Value {
-    Value(Rc::new(_Value {
-        data: x.get_data() + y.get_data(),
-        grad: Cell::new(0.0),
-        done: Cell::new(false),
-        op: Op::Mul(x.clone(), y.clone()),
-    }))
-}
-
-fn neg(x: &Value) -> Value {
-    mul(x, &Value::new(-1.0))
-}
+use micrograd_rs::*;
 
 fn main() {
     let a = Value::new(10.0);
@@ -84,45 +7,9 @@ fn main() {
     // let c = &mul(&a, &b);
     // let c = &add(&a, &b);
 
-    c.0.grad.set(1.0);
+    c.set_grad(1.0);
     c.backwards();
-    dbg!(c);
-    dbg!(a.0.grad.get());
-    dbg!(b.0.grad.get());
+    dbg!(a.grad());
+    dbg!(b.grad());
 }
 
-// use std::cell::Cell;
-
-// #[derive(Debug)]
-// struct Value<'a> {
-//     data: Cell<f64>,
-//     grad: Cell<f64>,
-//     children : Vec<&'a Value>,
-// }
-
-// impl Value<'_> {
-//     fn new(x: f64) -> Self {
-//         Self { data: Cell::new(x), grad: Cell::new(0.0), children: vec![] }
-//     }
-
-//     fn get(&self) -> f64 {
-//         self.data.get()
-//     }
-// }
-
-// fn add<'a>(x: &'a Value, y: &'a Value) -> Value<'a> {
-//     let sum = x.get() + y.get();
-//     Value { data: Cell::new(sum), grad: Cell::new(0.0), children: vec![] }
-// }
-
-// fn neg<'a>(x: &'a Value) -> Value<'a> {
-//     // return Value::new(-x.get());
-//     Value { data: Cell::new(-x.get()), grad: Cell::new(0.0), children: vec![] }
-// }
-
-// fn main() {
-//     let a = Value::new(10.0);
-//     let b = Value::new(-2.0);
-//     let c = add(&a, &add(&a, &neg(&b)));
-//     dbg!(c);
-// }
